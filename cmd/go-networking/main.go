445d76db9eb869b092da6b2b4475443cb2fb9e0c
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-14 18:45:44 +0600

    Add protocol selection and testing functionality

--- cmd/go-networking/main.go
+++ cmd/go-networking/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rudrodip/go-networking/pkg"
+	http "github.com/rudrodip/go-networking/protocols/http"
+	tcp "github.com/rudrodip/go-networking/protocols/tcp"
+	udp "github.com/rudrodip/go-networking/protocols/udp"
+)
+
+var protocolMap = map[string]func(){
+	"tcp":  tcp.TestTCP,
+	"udp":  udp.TestUDP,
+	"http": http.TestHttp,
+}
 
 func main() {
-	fmt.Println("hello world")
+	for {
+		fmt.Println("Available protocols:")
+		for protocol := range protocolMap {
+			fmt.Printf(" -> %s\n", protocol)
+		}
+		protocol := pkg.GetUserInput("[enter \"exit\" to exit]Enter protocol: ")
+		if protocol == "exit" {
+			break
+		}
+		if test, ok := protocolMap[protocol]; ok {
+			test()
+		} else {
+			fmt.Println("Invalid protocol")
+		}
+	}
 }
